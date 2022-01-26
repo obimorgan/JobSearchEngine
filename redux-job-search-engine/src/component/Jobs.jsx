b2b19096a5c7a96b90ba5d4c0a5466e11562feb3
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-26 16:34:25 +0100

    starting to implement redux connection

--- redux-job-search-engine/src/component/Jobs.jsx
+++ redux-job-search-engine/src/component/Jobs.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom"
+import { FcLikePlaceholder, FcLike } from "react-icons/fc";
+import { connect } from 'react-redux'
+import { addToFavouriesAction } from '../redux/actions'
 
-const Jobs = ({ data }) => {
+
+
+const mapDispatchToProps = (dispatch) => ({
+    addToFavourites: (company_name) => { dispatch(addToFavouriesAction(company_name)) }
+})
+
+const Jobs = ({ data, addToFavourites }) => {
     const { title, company_name, candidate_required_location } = data
     return (
         <>
@@ -8,6 +17,10 @@ const Jobs = ({ data }) => {
                 <div className="job_details_body flex-column">
                     <div className="job_card m-auto">
                         <h3>{company_name}</h3>
+                        <div className="like_placeholder"
+                            onClick={() => addToFavourites(company_name)}
+                        ><FcLikePlaceholder /></div>
+                        <div className="liked"><FcLike /></div>
                     </div>
                     <div className="job_details d-flex justify-content-between mx-3">
                         <div className="flex-column">
@@ -32,4 +45,5 @@ const Jobs = ({ data }) => {
     )
 }
 
-export default Jobs
+export default connect(s => ({}), mapDispatchToProps)(Jobs)
+// export default Jobs
