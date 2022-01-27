99d2f81093b2e46bd32d8a162166b0f0ff36b684
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-27 17:53:09 +0100

    fixed undefined error state.

--- redux-job-search-engine/src/component/Home.jsx
+++ redux-job-search-engine/src/component/Home.jsx
@@ -4,11 +4,11 @@ import { Container } from 'react-bootstrap'
 import CategorySearch from './SearchCategory'
 import SearchBar from './SearchBar'
 import { connect } from 'react-redux'
-// import { Alert } from 'react-bootstrap'
+import { Alert } from 'react-bootstrap'
 
-// const mapStateToProps = state => ({
-//     errorStatus: state.results.errorStatus
-// })
+const mapStateToProps = state => ({
+    errorStatus: state.categorySearch.errorStatus
+})
 
 const Home = ({ errorStatus }) => {
 
@@ -16,22 +16,25 @@ const Home = ({ errorStatus }) => {
         <>
             <div className="wrapper">
                 <h1>Search Jobs!</h1>
-                {/* {
-                    errorStatus && (
-                        <Alert variant="danger">
-                            There is an error with retrieving data: {errorStatus}
-                        </Alert>
-                    )
-                } */}
                 <div className="search_container d-flex align-items-center mt-n5">
                     <CategorySearch />
                     <SearchBar />
                     <Liked />
                 </div>
+
             </div>
             <Container >
                 <hr />
-
+                {
+                    errorStatus && (
+                        <Alert variant="danger">
+                            There is an error with retrieving data: {errorStatus}
+                        </Alert>
+                    )
+                }
+                {/* <Alert variant="danger">
+                    There is an error with retrieving data: {errorStatus}
+                </Alert> */}
                 <div className="d-flex flex-wrap justify-content-between">
                     <Jobs />
                 </div>
@@ -40,5 +43,5 @@ const Home = ({ errorStatus }) => {
     )
 }
 
-// export default connect(mapStateToProps)(Home)
-export default Home
+export default connect(mapStateToProps)(Home)
+// export default Home
